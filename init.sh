c2508e946761f371245433eda0619fa8157d6f99
Froz1 <ZaurMam@inbox.ru>
2018-08-29 17:32:33 +0300

    Task 2.1.11

--- init.sh
+++ init.sh
@@ -1,6 +1,21 @@
+#!/usr/bin/env bash
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+#python /home/box/web/tests.py
 
-sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo rm /etc/gunicorn.d/hello.py
+sudo rm /etc/gunicorn.d/django.py
+
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
+
 sudo /etc/init.d/gunicorn restart
+
+#gunicorn -c /etc/gunicorn.d/hello.py hello:application
+
+cd /home/box/web/ask
+#gunicorn -b 0.0.0.0:8000  ask.wsgi:application
+#gunicorn -c /etc/gunicorn.d/django.py ask.wsgi:application &
+#gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
+#sudo gunicorn ask.wsgi:application
